3abccd612cfd2d843c5d46e4153f7329207b4d42
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-20 23:29:54 -0300

    created habits table with generation of range of days and filling of days

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
-// import { Habit } from "./components/Habit";
 import { Header } from "./components/Header";
+import { Summarytable } from "./components/SummaryTable";
 import "./styles/global.css";
 
 export function App() {
@@ -7,6 +7,7 @@ export function App() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
+        <Summarytable />
       </div>
     </div>
   );
