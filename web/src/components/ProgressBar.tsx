d7be19e826d6d6674ec84613c6dded21b807bd75
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-21 15:48:22 -0300

    feat: created customization in habit squares

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,15 +1,15 @@
 import * as Progress from "@radix-ui/react-progress";
 
-interface ProgressBarProps {
+interface iProgressBarProps {
   progress: number;
 }
 
-export function ProgressBar(props: ProgressBarProps) {
+export function ProgressBar({ progress }: iProgressBarProps) {
   return (
     <Progress.Root className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
       <Progress.Indicator
         className="h-3 rounded-xl bg-violet-700"
-        style={{ width: `${props.progress}%` }}
+        style={{ width: `${progress}%` }}
       />
     </Progress.Root>
   );
