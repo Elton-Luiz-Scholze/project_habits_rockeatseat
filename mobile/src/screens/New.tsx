d6ad60a1e99c2e709e4a7720491d4ea7d365df2e
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-21 21:13:59 -0300

    feat: confirm button created and New page layout finalized

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from "react-native";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
+import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
 
 const avaiableWeekDays = [
   "Domingo",
@@ -27,7 +35,10 @@ export function New() {
   }
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: 100 }}
+        showsVerticalScrollIndicator={false}
+      >
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl">
@@ -38,7 +49,11 @@ export function New() {
           Qual o seu comprometimento?
         </Text>
 
-        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 border-green-600" />
+        <TextInput
+          placeholder="Ex.: Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 border-green-600"
+        />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
@@ -51,6 +66,17 @@ export function New() {
             onPress={() => handleToggleWeekDay(index)}
           />
         ))}
+
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
