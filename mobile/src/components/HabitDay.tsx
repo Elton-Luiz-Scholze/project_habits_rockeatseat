7729ec2a861ac2d64668790b5102878fa6fb87c3
Elton Luiz Scholze <elscholze@gmail.com>
2023-01-21 21:40:15 -0300

    feat: created navigation and screen layout of habit screen

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,10 @@
-import { TouchableOpacity, Dimensions } from "react-native";
+import {
+  TouchableOpacity,
+  Dimensions,
+  TouchableOpacityProps,
+} from "react-native";
+
+interface iPropsHabitDay extends TouchableOpacityProps {}
 
 const weekDays = 7;
 const screenHorizontalPadding = (32 * 2) / 5;
@@ -7,12 +13,13 @@ export const dayMarginBetween = 8;
 export const daySize =
   Dimensions.get("screen").width / weekDays - (screenHorizontalPadding + 5);
 
-export function HabitDay() {
+export function HabitDay({ ...rest }: iPropsHabitDay) {
   return (
     <TouchableOpacity
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
       style={{ width: daySize, height: daySize }}
       activeOpacity={0.7}
+      {...rest}
     />
   );
 }
